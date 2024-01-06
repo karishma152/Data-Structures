8ef03812ff3a3db3b1373e5b94df8f85694d0b55
karishma152 <42795077+karishma152@users.noreply.github.com>
2024-01-06 17:45:14 +0530

    Add project files.

---
+++ DSAPracticeQuestion/Pattern1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAPracticeQuestion
+{
+    public class Pattern1
+    {
+        public Pattern1()
+        {
+            this.PrintingPattern();
+        }
+
+        /// <summary>
+        /// 1 4
+        /// 2 3
+        /// 3 2
+        /// 4 1
+        /// </summary>
+        public void PrintingPattern()
+        {
+
+        }
+    }
+}
---
+++ DSAPracticeQuestion/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DSAPracticeQuestion;
+
+Console.WriteLine("Data Strtucutes");
+
+Console.WriteLine("Pattern 1");
+Pattern1 obj = new();
