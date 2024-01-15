52097a586cedd4381afeaf67fa1765a1289cc097
karishma152 <42795077+karishma152@users.noreply.github.com>
2024-01-15 14:35:08 +0530

    updated kis with candies

--- DSAPractice/DSAPractice/LeetCode/ArraysString/KidWithCandies.cs
+++ DSAPractice/DSAPractice/LeetCode/ArraysString/KidWithCandies.cs
@@ -6,13 +6,13 @@
 
         public void KidsWithCandies()
         {
-            int[] candies = { 2, 3, 5, 1, 3 };
-            int extraCandies = 3;
+            int[] candies = { 2,8,7};
+            int extraCandies = 1;
             bool[] result = new bool[candies.Length];
             int index = candies.Max();
             for (int i = 0; i < candies.Length; i++)
             {
-                result[i] = true ? candies[i] * extraCandies >= index : false;
+                result[i] = true ? candies[i] + extraCandies >= index : false;
             }
             for (int i = 0; i < candies.Length; i++)
                 Console.WriteLine(result[i]);
