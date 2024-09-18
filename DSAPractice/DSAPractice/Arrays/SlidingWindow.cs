85fcf0ce1c84cb0a515c1cd0f0b3d2ef875f7519
karishma152 <42795077+karishma152@users.noreply.github.com>
2024-09-18 21:40:51 +0530

    Sliding Window

---
+++ DSAPractice/DSAPractice/Arrays/SlidingWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAPractice.Arrays
+{
+    public class SlidingWindow
+    {
+        public void SlidingWindowQue()
+        {
+            int[] arr = { 1, 0, 3, 5, 2 };
+            int n = 3, maxSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                maxSum += arr[i];
+            }
+            int tempSum = maxSum;
+            for (int i = n; i < arr.Length; i++)
+            {
+                tempSum = tempSum + arr[i] - arr[i - n];
+                maxSum = Math.Max(maxSum, tempSum);
+            }
+            Console.WriteLine("Maximum sum of " + n + " length of subarray is: " + maxSum);
+        }
+    }
+}
--- DSAPractice/DSAPractice/Program.cs
+++ DSAPractice/DSAPractice/Program.cs
@@ -1,35 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DSAPractice;
+using DSAPractice.Arrays;
 using DSAPractice.LeetCode.ArraysString;
 
 Console.WriteLine("Data Structures Practice");
 
-Console.WriteLine("Patterns");
-Patterns obj = new Patterns();
+//Console.WriteLine("Patterns");
+//Patterns obj = new Patterns();
+//Console.WriteLine("Pattern 1");
+//obj.PrintingPattern1();
+
+//Console.WriteLine("Pattern 2");
+//obj.PrintingPattern2();
+
+//Console.WriteLine("Merge two string Alternatively");
+//MergeStringsAlternatively mergeStringsAlternatively = new MergeStringsAlternatively();
+//mergeStringsAlternatively.MergeAlternately();
+
+//Console.WriteLine("GCD of Two Strings - Optimised");
+//GCDofTwoStrings gCDofTwoStrings = new GCDofTwoStrings();
+//gCDofTwoStrings.GCDofTwoStringsOptimised();
+
+//Console.WriteLine("GCD of Two Strings - Brute Force");
+//gCDofTwoStrings.GCDofTwoStringsBruteForce();
+
+//Console.WriteLine("Kid with Candies");
+//KidWithCandies kidWithCandies = new KidWithCandies();
+//kidWithCandies.KidsWithCandies();
 
 
-Console.WriteLine("Pattern 1");
-obj.PrintingPattern1();
+//Console.WriteLine("Can Place Flowers");
+//CanPlaceFlowers canPlaceFlowers = new CanPlaceFlowers();
+//canPlaceFlowers.CanPlaceFlowersOutput();
 
-Console.WriteLine("Pattern 2");
-obj.PrintingPattern2();
+//Console.WriteLine("Reverse Vowels in String");
+//ReverseVowels reverseVowels = new ReverseVowels();
+//reverseVowels.ReverseVowelsInString();
 
-Console.WriteLine("Merge two string Alternatively");
-MergeStringsAlternatively mergeStringsAlternatively = new MergeStringsAlternatively();
-mergeStringsAlternatively.MergeAlternately();
+Console.WriteLine("Sliding Window");
+SlidingWindow slidingWindow = new SlidingWindow();
+slidingWindow.SlidingWindowQue();
 
-Console.WriteLine("GCD of Two Strings - Optimised");
-GCDofTwoStrings gCDofTwoStrings = new GCDofTwoStrings();
-gCDofTwoStrings.GCDofTwoStringsOptimised();
-
-Console.WriteLine("GCD of Two Strings - Brute Force");
-gCDofTwoStrings.GCDofTwoStringsBruteForce();
-
-Console.WriteLine("Kid with Candies");
-KidWithCandies kidWithCandies = new KidWithCandies();
-kidWithCandies.KidsWithCandies();
-
-
-Console.WriteLine("Can Place Flowers");
-CanPlaceFlowers canPlaceFlowers = new CanPlaceFlowers();
-canPlaceFlowers.CanPlaceFlowersOutput();
