de1ae866f222feb3a98e91e0e2529ff405d10c69
karishma152 <42795077+karishma152@users.noreply.github.com>
2024-09-20 14:51:05 +0530

    Reverse vowels in string

---
+++ DSAPractice/DSAPractice/LeetCode/ArraysString/ReverseVowels.cs
@@ -0,0 +1,41 @@
+﻿namespace DSAPractice.LeetCode.ArraysString
+{
+    public class ReverseVowels
+    {
+        #region Public Methods
+
+        public void ReverseVowelsInString()
+        {
+            string s = "IceCream";
+            var chars = s.ToList();
+            List<char> template = new List<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            List<char> vowels = new();
+            int numberOfVowels = 0;
+            foreach (char c in s)
+            {
+                if (template.Contains(c))
+                {
+                    vowels.Add(c);
+                    numberOfVowels++;
+
+                }
+            }
+            for(int i=0;i<s.Length;i++)
+            {
+                if (template.Contains(s[i]))
+                {
+                    chars[i] = vowels[numberOfVowels - 1];
+                    numberOfVowels--;
+                }
+            }
+            s = chars.ToString();
+           foreach(char ch in chars)
+            {
+                Console.Write(ch);
+            }
+
+        }
+
+        #endregion
+    }
+}
