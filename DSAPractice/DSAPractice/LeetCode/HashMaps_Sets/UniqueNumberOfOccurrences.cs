95af51c427f48e5a34fa831d94356d52439ce55e
karishma152 <42795077+karishma152@users.noreply.github.com>
2024-09-22 01:52:31 +0530

    Unique Number O fOccurrences

---
+++ DSAPractice/DSAPractice/LeetCode/HashMaps_Sets/UniqueNumberOfOccurrences.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAPractice.LeetCode.HashMaps_Sets
+{
+    public class UniqueNumberOfOccurrences
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            foreach (int i in arr)
+            {
+
+                if (!result.ContainsKey(i))
+                {
+                    result.Add(i, 1);
+                }
+                else
+                {
+                    result[i] = result[i] + 1; ;
+                }
+            }
+            if (result.Values.Distinct().Count() == result.Values.Count())
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
