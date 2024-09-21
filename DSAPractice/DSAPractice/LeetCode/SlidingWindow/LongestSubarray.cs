e583f56b745b43828e8eb8796991258bbd531d9e
karishma152 <42795077+karishma152@users.noreply.github.com>
2024-09-22 01:17:33 +0530

    Longest SubArray

---
+++ DSAPractice/DSAPractice/LeetCode/SlidingWindow/LongestSubarray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAPractice.LeetCode.SlidingWindow
+{
+    public class LongestSubarray
+    {
+        public int LongestSubarray(int[] nums)
+        {
+            int minCount = 0, maxCount = 0, j = 0; ;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                    minCount++;
+                while (minCount > 1)
+                {
+                    if (nums[j] == 0)
+                    {
+                        minCount--;
+                    }
+                    j++;
+                }
+                maxCount = Math.Max(maxCount, i - j);
+            }
+            return maxCount;
+        }
+    }
+}
