a10efab3068a810ac1b00e95481f8dfcd39433cc
karishma152 <42795077+karishma152@users.noreply.github.com>
2024-09-22 01:14:50 +0530

    Maximum Number of Vowels

---
+++ DSAPractice/DSAPractice/LeetCode/SlidingWindow/MaximumNumberOfVowels.cs
@@ -0,0 +1,29 @@
+﻿namespace DSAPractice.LeetCode.SlidingWindow
+{
+    public class MaximumNumberOfVowels
+    {
+        public int MaxVowels(string s, int k)
+        {
+            int maxCount = 0, tempCount = 0;
+            char[] nums = s.ToCharArray();
+            char[] template = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            for (int i = 0; i < k; i++)
+            {
+                if (template.Contains(nums[i]))
+                    maxCount++;
+            }
+            tempCount = maxCount;
+            for (int i = k; i < nums.Length; i++)
+            {
+                if (template.Contains(nums[i - k]))
+                    tempCount--;
+                if (template.Contains(nums[i]))
+                {
+                    tempCount = tempCount + 1;
+                }
+                maxCount = Math.Max(tempCount, maxCount);
+            }
+            return maxCount;
+        }
+    }
+}
