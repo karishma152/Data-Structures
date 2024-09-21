8e1890da8c2ddbea32979fc1a688aae2d004d513
karishma152 <42795077+karishma152@users.noreply.github.com>
2024-09-22 01:13:21 +0530

    Maximum SubArray Average

---
+++ DSAPractice/DSAPractice/LeetCode/SlidingWindow/MaximumAverageSubarray1.cs
@@ -0,0 +1,22 @@
+﻿namespace DSAPractice.LeetCode.SlidingWindow
+{
+    public class MaximumAverageSubarray1
+    {
+        public double MaxAvgSubArray(int[] nums, int k)
+        {
+            double maxAvgVal = 0, tempVal = 0;
+            for (int i = 0; i < k; i++)
+            {
+                maxAvgVal += nums[i];
+            }
+            maxAvgVal = maxAvgVal;
+            tempVal = maxAvgVal;
+            for (int i = k; i < nums.Length; i++)
+            {
+                tempVal = tempVal + nums[i] - nums[i - k];
+                maxAvgVal = Math.Max(tempVal, maxAvgVal);
+            }
+            return maxAvgVal / k;
+        }
+    }
+}
