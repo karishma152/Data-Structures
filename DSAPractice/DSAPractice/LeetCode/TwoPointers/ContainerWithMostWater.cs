6ce46517920f2bc7c1b4e0d9dbe2042ec2a9e793
karisma jain <karishmajain008007@gmail.com>
2024-10-06 06:48:47 +0530

    Two Pointers  - maxArea and Sum pairs

---
+++ DSAPractice/DSAPractice/LeetCode/TwoPointers/ContainerWithMostWater.cs
@@ -0,0 +1,32 @@
+﻿namespace DSAPractice.LeetCode.TwoPointers
+{
+    public class ContainerWithMostWater
+    {
+        //check if lenght of height is 0, if yes return -1
+        //initialise two variable left=0 and right = heigt.Length -1
+        //calculate are between those two
+        //if right is greate than left, increase left, else increase right
+        //calculate the area then and find out the max area
+        public int MaxArea()
+        {
+            int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            if (height.Length == 0)
+                return 0;
+            int left = 0, right = height.Length - 1, maxArea = 0;
+            while (left < right && right < height.Length)
+            {
+                int length = right - left;
+                int minWall = Math.Min(height[left], height[right]);
+                int area = length * minWall;
+                maxArea = Math.Max(area, maxArea);
+                if (height[right] > height[left])
+                    left++;
+                else
+                    right--;
+
+            }
+            return maxArea;
+
+        }
+    }
+}
---
+++ DSAPractice/DSAPractice/LeetCode/TwoPointers/MaxNumberKSumPairs.cs
@@ -0,0 +1,34 @@
+﻿namespace DSAPractice.LeetCode.TwoPointers
+{
+    public class MaxNumberKSumPairs
+    {
+        //sort the array and check if array.Length>0, if not return 0
+        //initalize three integers left = 0, right = nums.Length -1 ans ans = 0
+        //check if left + right == k if yes then increment left and decrease right, and increment ans
+        //else if left + right > k, right -- 
+        //else if left + right < k, left ++;
+        public int MaxOperations()
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            int k = 5;
+            int left = 0, right = nums.Length - 1, ans = 0;
+            if (nums.Length == 0)
+                return ans;
+            Array.Sort(nums);
+            while (left < right)
+            {
+                if (nums[left] + nums[right] == k)
+                {
+                    left++;
+                    right--;
+                    ans++;
+                }
+                else if (nums[left] + nums[right] > k)
+                    right--;
+                else if (nums[left] + nums[right] < k)
+                    left++;
+            }
+            return ans;
+        }
+    }
+}
